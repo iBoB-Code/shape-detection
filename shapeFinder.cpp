470cbeb2d8fe8860a617d66c632b41b6075fa290
Sebas Hollow <Sebas.V.Hollow@gmail.com>
2017-10-24 01:02:35 +0900

    Code cleanup
    Code cleanup & formatting, moved images to a separate dir

--- shapeFinder.cpp
+++ shapeFinder.cpp
@@ -1,4 +1,3 @@
-#include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
 #include <iostream>
@@ -11,78 +10,89 @@ char window_name1[] = "Unprocessed Image";
 char window_name2[] = "Processed Image";
 
 void imagePreprocessing() {
-  // Make it bi-color for more precision
+    // Make it bi-color for more precision
 }
 
-float deformationIndice(int i, int a, int b, int c) {
-  float sba = std::sqrt(std::pow((step + i) - i, 2) + std::pow(b - a, 2));
-  float sbc = std::sqrt(std::pow((step + i) - ((step * 2) + i), 2) + std::pow(b - c, 2));
-  float sac = std::sqrt(std::pow(i - ((step * 2) + i), 2) + std::pow(a - c, 2));
-  float angle = (std::pow(sba, 2) + std::pow(sbc, 2) - std::pow(sac, 2)) / (2 * sba * sbc);
-  int value = std::acos(angle) * (180/3.14);
-  return value;
-}
+float deformationIndice(int i, int a, int b, int c)
+    {
+    const float sba = sqrt (pow (step, 2) + pow (b - a, 2));
+    const float sbc = sqrt (pow (step + i - (step * 2 + i), 2) + pow (b - c, 2));
+    const float sac = sqrt (pow (i - (step * 2 + i), 2) + pow (a - c, 2));
+    const float angle = (pow (sba, 2) + pow (sbc, 2) - pow (sac, 2)) / (2 * sba * sbc);
 
-int spikesCounter(std::vector<int>& v) {
-  int sum = 0;
-  for(std::vector<int>::size_type i = 0; i != v.size(); i++) {
-    sum += v[i];
-  }
-  sum /= v.size();
-  int limit = sum - (sum / 10);
-  int spikes = 0;
-  bool isUnder = false;
-  for(std::vector<int>::size_type i = 0; i != v.size(); i++) {
-    if (v[i] < limit && !isUnder) {
-      spikes++;
-      isUnder = true;
+    return acos(angle) * (180 / 3.14);
     }
-    if (v[i] > limit && isUnder) {
-      isUnder = false;
-    }
-  }
-  return spikes;
+
+int spikesCounter(vector<int>& v)
+    {
+    int sum = 0;
+    for(vector<int>::size_type i = 0; i != v.size(); i++)
+        sum += v[i];
+
+    sum /= v.size();
+    
+    int limit = sum - (sum / 10);
+    int spikes = 0;
+    bool isUnder = false;
+
+    for(vector<int>::size_type i = 0; i != v.size(); i++)
+        {
+        if (v[i] < limit && !isUnder) 
+            {
+            spikes++;
+            isUnder = true;
+            }
+
+        if (v[i] > limit && isUnder)
+            isUnder = false;
+        }
+
+    return spikes;
 }
 
 void imageAnalyse() {
-  std::vector<int> scan;
-  step =  (10 * img.cols) / 100;
-  for (int i = 0; i < img.rows; ++i)
-  {
-      cv::Vec3b* pixel = img.ptr<cv::Vec3b>(i);
-      int fw = 0;
-      int bw = img.cols - 1;
-      while (fw < img.cols && pixel[fw][2] == 255) {
-        fw++;
-      }
-      while (bw >= 0 && pixel[bw][2] == 255) {
-        bw--;
-      }
-      if (bw - fw > 0) {
-        scan.push_back(bw - fw);
-      }
-  }
+    vector<int> scan;
+    step =  10 * img.cols / 100;
 
-  bool isFlat = true;
-  int tmp = scan[0];
-  std::vector<int> dE;
-  for(std::vector<int>::size_type i = 0; i != scan.size(); i++) {
-    if (scan[i] != tmp)
-      isFlat = false;
-    if (i + (step * 2) < scan.size())
-      dE.push_back(deformationIndice(i, scan[i], scan[i + step], scan[i + (step * 2)]));
-  }
-  int sp = spikesCounter(dE);
-  if (sp == 0 && !isFlat)
-    std::cout << "CIRCLE !!" << std::endl;
-  else
-    std::cout << "SQUARE !!" << std::endl;
-}
+    for (int i = 0; i < img.rows; ++i)
+        {
+        Vec3b* pixel = img.ptr<Vec3b>(i);
+        int fw = 0;
+        int bw = img.cols - 1;
+
+        while (fw < img.cols && pixel[fw][2] == 255)
+            fw++;
+
+        while (bw >= 0 && pixel[bw][2] == 255)
+            bw--;
+
+        if (bw - fw > 0)
+            scan.push_back(bw - fw);
+        }
+    
+    bool isFlat = true;
+    int tmp = scan[0];
+    vector<int> dE;
+    for (vector<int>::size_type i = 0; i != scan.size(); i++)
+        {
+        if (scan[i] != tmp)
+            isFlat = false;
+
+        if (i + step * 2 < scan.size())
+            dE.push_back(deformationIndice(i, scan[i], scan[i + step], scan[i + (step * 2)]));
+        }
+
+    int sp = spikesCounter(dE);
+    if (sp == 0 && !isFlat)
+        std::cout << "CIRCLE !!" << std::endl;
+    else
+        std::cout << "SQUARE !!" << std::endl;
+    }
 
 int main( int argc, char** argv )
-{
+    {
     img = imread( argv[1], 1 );
     imagePreprocessing();
     imageAnalyse();
     return 0;
-}
+    }
