6c9fb9a7ea5541a62c699285c505135b957ac530
Sebas Hollow <Sebas.V.Hollow@gmail.com>
2017-10-24 00:34:44 +0900

    Ported to VS

--- CMakeFiles/BlurImage.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "BlurImage.pdb"
-  "BlurImage"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BlurImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
