c3cd51b5923727aa852b34736121b7c825122bd5
Sebas Hollow <Sebas.V.Hollow@gmail.com>
2017-10-24 20:19:17 +0900

    Updated ShapeFinder doc, included pdf file version as well, fixed script files.

--- environment-prewire.bat
+++ environment-prewire.bat
@@ -1,7 +1,7 @@
 
 @echo off
 
-set OPENCV_dir=%~dp0OpenCV\build\x64\vc14
+set OPENCV_dir=%~dp0\Dependencies\OpenCV\build\x64\vc14
 set PATH=%OPENCV_dir%\bin\;%PATH%
 
 @echo on
--- execute.bat
+++ execute.bat
@@ -2,12 +2,11 @@ setlocal
 
 call "%~dp0\environment-prewire.bat"
 
-set PROJECT_exe="%~dp0shape-detection\x64\Debug\Shape detection.exe"
-set IMAGES_dir=%~dp0shape-detection\Images
+set PROJECT_exe="Project\x64\Debug\Shape detection.exe"
 
-%PROJECT_exe% "%IMAGES_dir%\circle.png"
-%PROJECT_exe% "%IMAGES_dir%\square1.png"
-%PROJECT_exe% "%IMAGES_dir%\square2.png"
-%PROJECT_exe% "%IMAGES_dir%\square3.png"
+%PROJECT_exe% Images\circle.png
+%PROJECT_exe% Images\square1.png
+%PROJECT_exe% Images\square2.png
+%PROJECT_exe% Images\square3.png
 
 endlocal
